6bf54609960a866c862fd16c3abc7647eef6dc51
Petri Kallio <kallionpetri@gmail.com>
2017-04-02 10:09:03 +0300

    Lisäyslomake lisätty

--- src/main/java/refApp/controllers/ReferenceController.java
+++ src/main/java/refApp/controllers/ReferenceController.java
@@ -1,8 +1,11 @@
 
 package refApp.controllers;
 
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * A controller to handle requests based on references
@@ -14,5 +17,12 @@ public class ReferenceController {
     @RequestMapping("/")
     public String home() {
         return "references/new";
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String add(@RequestParam Map<String, String> params) {
+        System.out.println(params.get("author") + ": " + params.get("name") 
+                + "; " + params.get("year"));
+        return "redirect:/";
     }
 }
