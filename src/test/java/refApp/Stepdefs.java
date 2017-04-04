412b1dbaad4912701f430cab61ea836363f0dbb0
Henrik Lyra <henriklyra@gmail.com>
2017-04-04 17:40:01 +0300

    rikkinäinen cucumber

--- src/test/java/refApp/Stepdefs.java
+++ src/test/java/refApp/Stepdefs.java
@@ -1,6 +1,6 @@
 package refApp;
 
-
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -11,18 +11,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Stepdefs {
-    
-    WebDriver driver = new FirefoxDriver();
 
-    private void createReference(String title, String auth, String publisher, String year, String pages_start, String pages_end, String address) {
-        assertTrue(driver.getPageSource().contains(""));
+    WebDriver driver = new FirefoxDriver();
+    String baseUrl = "localhost:8080";
+
+    @Given("^add new is selected$")
+    public void add_new_is_selected() throws Throwable {
+        driver.get(baseUrl);
+    }
+
+    @When("^correct title \"([^\"]*)\" and author \"([^\"]*)\" and publisher \"([^\"]*)\" and year \"([^\"]*)\"  are added$")
+    public void correct_title_and_author_and_year(String title, String author, String publisher, String year, String pages_start, String pages_end, String address) {
+        createReference(title, author, year);
+    }
+
+    @Then("^form is submitted and list of references is displayed$")
+    public void form_is_submitted_and_list_of_references_is_displayed() throws Throwable {
+        pageHasContent("Referenz-o-matic 3000");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    /* helper methods */
+    private void pageHasContent(String content) {
+        assertTrue(driver.getPageSource().contains(content));
+    }
+
+    private void createReference(String title, String auth, String year) {
+        assertTrue(driver.getPageSource().contains("Lisää uusi kirja"));
         WebElement element = driver.findElement(By.name("name"));
         element.sendKeys(title);
         element = driver.findElement(By.name("author"));
         element.sendKeys(auth);
         element = driver.findElement(By.name("year"));
         element.sendKeys(year);
-        element = driver.findElement(By.name("publisher"));
+    /*    element = driver.findElement(By.name("publisher"));
         element.sendKeys(publisher);
         element = driver.findElement(By.name("pages_start"));
         element.sendKeys(pages_start);
@@ -30,7 +56,7 @@ public class Stepdefs {
         element.sendKeys(pages_end);
         element = driver.findElement(By.name("String address"));
         element.sendKeys(address);
-        element = driver.findElement(By.name("submit"));
-        element.sendKeys();
+        element = driver.findElement(By.name("submit"));   */
+        element.submit();
     }
 }
--- src/test/resources/refApp/addingBook.feature
+++ src/test/resources/refApp/addingBook.feature
@@ -1,12 +1,6 @@
 Feature: As a user i want to add a book easily so that the reference is saved
 
-#Scenario: user can add a new book reference
-#   Given add new is selected
-#    When correct author "Tero Testi" is added
-#    And corret title "Testikirja" is added
-#    And correct publisher "Wsoy" is added
-#    And correct year "2017" is added
-#    And correct address "testiosoite" is added
-#    And correct first page "2" is added
-#    And correct last page "23" is added
-#    And form is submittedcd
+Scenario: user can add a new book reference
+    Given add new is selected
+    When correct title "Testikirja" and author "Tero Testi" and publisher "Wsoy" and year "2017" are added
+    Then form is submitted and list of references is displayed
