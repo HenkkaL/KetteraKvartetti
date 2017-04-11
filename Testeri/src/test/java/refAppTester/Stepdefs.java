dd61a9899c0ad39b1de8d66b47c41a7bf3f48c27
Henrik Lyra <henriklyra@gmail.com>
2017-04-11 14:50:18 +0300

    stepdefsiin apumetodeja

--- Testeri/src/test/java/refAppTester/Stepdefs.java
+++ Testeri/src/test/java/refAppTester/Stepdefs.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.By;
 //import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Stepdefs {
 
@@ -24,15 +25,27 @@ public class Stepdefs {
         driver.get(baseUrl);
     }
 
-    @Given("^add new is selected$")
-    public void add_new_is_selected() throws Throwable {
+    @Given("^add new \"([^\"]*)\" is selected$")
+    public void add_new_book_is_selected(String refType) throws Throwable {
         driver.get(baseUrl);
+        chooseRefType(refType);
     }
+
+    @Given("^add new is selected$")
+    public void add_new_is_selected(String refType) throws Throwable {
+        driver.get(baseUrl);
+        chooseRefType(refType);
+    }    
 
     @When("^correct referenceId \"([^\"]*)\" and title \"([^\"]*)\" and author \"([^\"]*)\" and publisher \"([^\"]*)\" and year \"([^\"]*)\" are added$")
     public void correct_referenceId_and_title_and_author_and_publisher_and_year_are_added(String referenceId, String title, String author, String publisher, String year) throws Throwable {
-        createReference(referenceId, title, author, year, publisher);
-
+        //createReference(referenceId, title, author, year, publisher);
+        this.addReferenceId(referenceId);
+        this.addTitle(title);
+        this.addAuthor(author);
+        this.addYear(year);
+        this.addPublisher(publisher);
+        this.makeSubmit();
     }
 
     @Then("^form is submitted and list of references is displayed$")
@@ -50,10 +63,8 @@ public class Stepdefs {
         assertTrue(driver.getPageSource().contains(content));
     }
 
-    private void createReference(String reference_id, String title, String auth, String year, String publisher) {
-//        try{ Thread.sleep(3000); } catch(Exception e){}  // suoritus pysähtyy 3 sekunniksi
+    /*private void createReference(String reference_id, String title, String auth, String year, String publisher) {
         pageHasContent("Lisää uusi viite");
-//        assertTrue(driver.getPageSource().contains("Lisää uusi viite"));
         WebElement element = driver.findElement(By.name("title"));
         element.sendKeys(title);
         element = driver.findElement(By.name("reference_id"));
@@ -65,6 +76,73 @@ public class Stepdefs {
         element = driver.findElement(By.name("publisher"));
         element.sendKeys(publisher);
         element = driver.findElement(By.name("submit"));
+        element.submit();
+    }*/
+
+    private void chooseRefType(String refType) {
+        Select dropdown = new Select(driver.findElement(By.id("type")));
+        dropdown.selectByValue(refType);
+    }
+
+    private void addReferenceId(String refId) {
+        WebElement element = driver.findElement(By.name("reference_id"));
+        element.sendKeys(refId);
+    }
+
+    private void addTitle(String title) {
+        WebElement element = driver.findElement(By.name("title"));
+        element.sendKeys(title);
+    }
+
+    private void addAuthor(String author) {
+        WebElement element = driver.findElement(By.name("author"));
+        element.sendKeys(author);
+    }
+
+    private void addYear(String year) {
+        WebElement element = driver.findElement(By.name("year"));
+        element.sendKeys(year);
+    }
+
+    private void addMonth(String month) {
+        WebElement element = driver.findElement(By.name("month"));
+        element.sendKeys(month);
+    }
+
+    private void addPublisher(String publisher) {
+        WebElement element = driver.findElement(By.name("publisher"));
+        element.sendKeys(publisher);
+    }
+
+    private void addEdition(String edition) {
+        WebElement element = driver.findElement(By.name("edition"));
+        element.sendKeys(edition);
+    }
+
+    private void addVolume(String volume) {
+        WebElement element = driver.findElement(By.name("volume"));
+        element.sendKeys(volume);
+    }
+
+    private void addSeries(String series) {
+        WebElement element = driver.findElement(By.name("series"));
+        element.sendKeys(series);
+    }
+
+    private void addAddress(String address) {
+        WebElement element = driver.findElement(By.name("address"));
+        element.sendKeys(address);
+    }
+
+    private void addNote(String note) {
+        WebElement element = driver.findElement(By.name("note"));
+        element.sendKeys(note);
+    }
+
+    //sivunumeroiden lisäys puuttuu
+    
+    private void makeSubmit( ) {
+        WebElement element = driver.findElement(By.name("submit"));
         element.submit();
     }
 }
--- Testeri/src/test/resources/refAppTester/addingBook.feature
+++ Testeri/src/test/resources/refAppTester/addingBook.feature
@@ -1,6 +1,7 @@
 Feature: As a user i want to add a book easily so that the reference is saved
 
 Scenario: user can add a new book reference
-    Given add new is selected
+    Given add new "book" is selected
     When correct referenceId "refId" and title "Testikirja" and author "Tero Testi" and publisher "Wsoy" and year "2017" are added
     Then form is submitted and list of references is displayed
+
