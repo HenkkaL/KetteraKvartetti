19fa01eab178d574d11c1de3cbb332ff59cb6d53
Henrik Lyra <henriklyra@gmail.com>
2017-04-25 09:53:12 +0300

    cucumber testejä korjattu

--- Testeri/src/test/java/refAppTester/Stepdefs.java
+++ Testeri/src/test/java/refAppTester/Stepdefs.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 //import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -20,93 +21,92 @@ public class Stepdefs {
     WebDriver driver = new HtmlUnitDriver();
 //    WebDriver driver = new ChromeDriver();
     String baseUrl = "http://kettera-kvartetti-test.herokuapp.com";
+//  String baseUrl = "http://localhost:8080";
 
     @Before
     public void alustus() {
         driver.get(baseUrl);
     }
 
-
     @Given("^add new \"([^\\\"]*)\" is selected$")
     public void add_new_is_selected(String refType) throws Throwable {
         driver.get(baseUrl);
         chooseRefType(refType);
-    }   
-    
-    @When("^correct referencId \"([^\"]*)\" is given")
-    public void correct_referenceId_is_given(String referenceId) {
-        this.addReferenceId(referenceId);
     }
-    
+
+    //   @When("^correct referencId \"([^\"]*)\" is given")
+    //   public void correct_referenceId_is_given(String referenceId) {
+    //       this.addReferenceId(referenceId);
+    //   }
     @When("^correct title \"([^\"]*)\" is given")
     public void correct_title_is_given(String title) {
         this.addTitle(title);
-    }    
-    
+    }
+
     @When("^correct author \"([^\"]*)\" is given")
     public void correct_author_is_given(String author) {
         this.addAuthor(author);
-    }        
-    
+    }
+
     @When("^correct year \"([^\"]*)\" is given")
     public void correct_year_is_given(String year) {
         this.addYear(year);
-    }    
-    
+    }
+
     @When("^correct publisher \"([^\"]*)\" is given")
     public void correct_publisher_is_given(String publisher) {
         this.addPublisher(publisher);
-    }        
+    }
 
     @When("^correct month \"([^\"]*)\" is given")
     public void correct_month_is_given(String month) {
         this.addMonth(month);
-    }       
+    }
 
     @When("^correct edition \"([^\"]*)\" is given")
     public void correct_edition_is_given(String edition) {
         this.addEdition(edition);
-    }       
+    }
 
     @When("^correct volume \"([^\"]*)\" is given")
     public void correct_volume_is_given(String volume) {
         this.addVolume(volume);
-    }       
+    }
 
     @When("^correct series \"([^\"]*)\" is given")
     public void correct_series_is_given(String series) {
         this.addSeries(series);
-    }       
+    }
 
     @When("^correct address \"([^\"]*)\" is given")
     public void correct_address_is_given(String address) {
         this.addAddress(address);
-    }       
+    }
 
     @When("^correct note \"([^\"]*)\" is given")
     public void correct_note_is_given(String note) {
         this.addNote(note);
-    }   
-    
+    }
+
     @When("^correct book_title \"([^\"]*)\" is given")
     public void correct_book_title_is_given(String book_title) {
         this.addBookTitle(book_title);
-    }   
+    }
 
     @When("^correct editor \"([^\"]*)\" is given")
     public void correct_editor_is_given(String editor) {
         this.addEditor(editor);
     }
-    
+
     @When("^correct volume_number \"([^\"]*)\" is given")
     public void correct_volume_number_is_given(String volume_number) {
         this.addVolumeNumber(volume_number);
-    }    
-    
+    }
+
     @When("^correct organisation \"([^\"]*)\" is given")
     public void correct_organisation_is_given(String organisation) {
         this.addVolumeNumber(organisation);
-    }    
+    }
 
     @When("^correct journal \"([^\"]*)\" is given")
     public void correct_journal_is_given(String journal) {
@@ -117,24 +117,22 @@ public class Stepdefs {
     public void correct_number_is_given(String number) {
         this.addNumber(number);
     }
-    
+
     @When("^correct pages start \"([^\"]*)\" and end \"([^\"]*)\" are given")
     public void correct_pages_are_given(String start, String end) {
         this.addPages(start, end);
-    }      
-    
+    }
+
     @When("^form is submitted")
     public void submit() {
-        //this.makeSubmit();
-        WebElement element = driver.findElement(By.name("submit"));
-        element.click();
-    }    
-    
+        this.makeSubmit();
+    }
+
     @Then("^page has content \"([^\"]*)\"")
     public void find_page_content_displayed(String content) throws Throwable {
         pageHasContent(content);
     }
-    
+
     @After
     public void tearDown() {
         driver.quit();
@@ -144,7 +142,7 @@ public class Stepdefs {
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
     }
-   
+
     private void chooseRefType(String refType) {
         Select dropdown = new Select(driver.findElement(By.id("type")));
         dropdown.selectByValue(refType);
@@ -204,46 +202,61 @@ public class Stepdefs {
         WebElement element = driver.findElement(By.name("note"));
         element.sendKeys(note);
     }
-    
+
     private void addBookTitle(String bookTitle) {
         WebElement element = driver.findElement(By.name("book_title"));
         element.sendKeys(bookTitle);
-    }    
-    
+    }
+
     private void addEditor(String editor) {
         WebElement element = driver.findElement(By.name("editor"));
         element.sendKeys(editor);
-    }      
+    }
 
     private void addVolumeNumber(String volume_number) {
         WebElement element = driver.findElement(By.name("volume_number"));
         element.sendKeys(volume_number);
-    }          
-    
+    }
+
     private void addOrganisation(String organisation) {
         WebElement element = driver.findElement(By.name("organisation"));
         element.sendKeys(organisation);
-    }    
-    
+    }
+
     private void addJournal(String journal) {
         WebElement element = driver.findElement(By.name("journal"));
         element.sendKeys(journal);
-    }        
-    
+    }
+
     private void addNumber(String number) {
         WebElement element = driver.findElement(By.name("number"));
         element.sendKeys(number);
-    }        
-    
+    }
+
     private void addPages(String pagesStart, String pagesEnd) {
         WebElement element = driver.findElement(By.name("pages_start"));
         element.sendKeys(pagesStart);
         element = driver.findElement(By.name("pages_end"));
         element.sendKeys(pagesEnd);
-    }    
-    
-    private void makeSubmit( ) {
-        WebElement element = driver.findElement(By.name("submit"));
-        element.click();
+    }
+
+    private void makeSubmit() {
+
+        if (checkValidation()) {
+            WebElement element = driver.findElement(By.name("submit"));
+            element.submit();
+        }
+
+    }
+
+    private boolean checkValidation() {
+        WebElement element;
+        try {
+            element = driver.findElement(By.cssSelector("required=required"));
+        } catch (NoSuchElementException e) {
+            return true;
+        }
+        return false;
+
     }
 }
