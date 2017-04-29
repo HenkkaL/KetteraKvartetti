da6b76433a22a20009462a58c5b4d3bb26658ba5
Petri Kallio <kallionpetri@gmail.com>
2017-04-29 13:35:04 +0300

    test_deploy päivitetty vielä kerran

--- test_deploy.sh
+++ test_deploy.sh
@@ -2,26 +2,35 @@
 
 commit=$(git log | head -n 5 | tail -n 1 | sed -e 's/^[[:space:]]*//')
 
-echo "N.B. Database is not yet cleared since it's not in use."
-echo "Uncomment the necessary lines from this script when Postgres-database is up and running."
+echo
+echo "------------------------------------------"
+echo "|                                        |"
+echo "|           KETTERÄ  KVARTETTI           |"
+echo "|                                        |"
+echo "|             test deploy to             |"
+echo "|  kettera-kvartetti-test.herokuapp.com  |"
+echo "|                                        |"
+echo "------------------------------------------"
+echo
 
-echo "Clearing test database..."
+echo " 1. Clearing test database..."
 echo
 heroku pg:reset --remote heroku-test --confirm kettera-kvartetti-test
 
 echo
-echo "Pushing latest commit $commit to heroku-test remote..."
+echo " 2. Pushing latest commit $commit to heroku-test remote..."
 echo
 
 git push heroku-test master
 
 echo
-echo "Running Cucumber tests..."
+echo " 3. Running Cucumber tests..."
+echo
 cd Testeri
-echo "Changed working directory to $(pwd)."
+echo "    Changed working directory to $(pwd)."
 
 gradle cleanTest test
 
 echo
-echo "Done."
+echo " DONE."
 echo
