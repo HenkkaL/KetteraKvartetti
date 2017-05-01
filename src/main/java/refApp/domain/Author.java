290e7b906a0b63af0c8e40ce01a94238fb1dc5d1
RIR <raine.rantanen@helsinki.fi>
2017-05-01 14:32:07 +0300

    minisiistiminen ja init-pusku heroku resetin jälkeen

--- src/main/java/refApp/domain/Author.java
+++ src/main/java/refApp/domain/Author.java
@@ -3,14 +3,10 @@ package refApp.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import org.springframework.data.jpa.domain.AbstractPersistable;
 
 /**
  * Class for author
