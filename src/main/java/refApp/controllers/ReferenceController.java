beeae1f645f2ceb3a73a495e8c48aea5a5bf4c6d
Henrik Lyra <henriklyra@gmail.com>
2017-05-03 11:31:09 +0300

    muokkaustoimintoa aloitettu

--- src/main/java/refApp/controllers/ReferenceController.java
+++ src/main/java/refApp/controllers/ReferenceController.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,6 +37,13 @@ public class ReferenceController {
     public String add(@RequestParam Map<String, String> params) {
         this.referenceService.addReference(params);
         return "redirect:/list_all";
+    }
+    
+    @RequestMapping(value = "/references/{referenceId}", method = RequestMethod.GET)
+    public String update(@PathVariable ("referenceId") Long refId, Model model) {
+        System.out.println("Testitulostus " + refId);
+        model.addAttribute("reference", this.referenceService.getReferenceRepo().findOne(refId));
+        return "references/reference";
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
